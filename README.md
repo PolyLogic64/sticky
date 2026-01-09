f00aa2edbd88c431cc4a366f4705030e879eda2e
PolyLogic64 <50244580+PolyLogic64@users.noreply.github.com>
2026-01-09 19:30:52 +0100

    added help, refactored, docs

---
+++ README.md
@@ -0,0 +1,8 @@
+# Sticky
+
+> [!CAUTION]
+> This program is in development and updates might do breaking changes.
+> Currently this is only being developed for @PolyLogic64 and his streams.
+> Development is happening [on stream](https://www.twitch.tv/Polylogic).
+
+A program to easily modify checklists to be displayed on stream.
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ ItemDict = dict[str, str | bool]
 class Item:
     title: str
     done: bool = False
+    hidden: bool = False
 
 
 def item_to_string(item: Item) -> str:
@@ -22,10 +23,18 @@ def item_to_string(item: Item) -> str:
 
 
 def item_to_dict(item: Item) -> ItemDict:
-    return {"title": item.title, "done": item.done}
+    return {"title": item.title, "done": item.done, "hidden": item.hidden}
+
+
+def dict_to_item(d: dict) -> Item:
+    return Item(
+        done=d.get("done", False), title=d["title"], hidden=d.get("hidden", False)
+    )
 
 
 def load_checklist_from_savefile(savefile_path: Path) -> list[Item]:
+    logger = logging.getLogger("load_checklist_from_savefile")
+
     checklist: list[Item] = []
     with savefile_path.open("r", encoding="utf-8", newline="\n") as f:
         deserialized = json.load(f)  # type: ignore
@@ -33,7 +42,8 @@ def load_checklist_from_savefile(savefile_path: Path) -> list[Item]:
 
         for thing in deserialized:
             assert isinstance(thing, dict)
-            checklist.append(Item(done=thing["done"], title=thing["title"]))
+            checklist.append(dict_to_item(thing))
+    logger.debug(f"Loaded Checklist: {checklist!r}")
     return checklist
 
 
@@ -58,6 +68,8 @@ def save_checklist(checklist: list[Item]) -> None:
         f.write("=== CHECKLIST ===\n")
 
         for item in checklist:
+            if item.hidden:
+                continue
             f.write(f"{item_to_string(item)}\n")
 
     logger.debug(f"Written file '{filename}'")
@@ -89,6 +101,19 @@ def get_index_from_command_argument(splitted_string: list[str]) -> int | None:
         return None
 
 
+def print_usage() -> None:
+    print()
+    print("REPL Commands:")
+    print("  help        -- print this help message")
+    print("  exit        -- exit the program")
+    print("  add <name>  -- appends a new item to the checklist")
+    print("  mark <id>   -- marks an item as completed")
+    print("  delete <id> -- deletes an item from the checklist")
+    print("  save        -- save the checklist manually")
+    print("  clean       -- 'delete' all marked items")
+    print()
+
+
 def actual_main(savefile_path: Path) -> None:
     logger = logging.getLogger("main")
 
@@ -97,10 +122,13 @@ def actual_main(savefile_path: Path) -> None:
         return
 
     checklist = load_checklist_from_savefile(savefile_path)
+    print_usage()
 
     # Loop
     while True:
         for pos, item in enumerate(checklist):
+            if item.hidden:
+                continue
             print(f"{pos:>2}  {item_to_string(item)}")
 
         # Read
@@ -139,8 +167,16 @@ def actual_main(savefile_path: Path) -> None:
                 logger.error("The index is out of bounds")
                 continue
 
-            del checklist[index]
+            checklist[index].hidden = True
 
+        elif command == "help":
+            print_usage()
+        elif command == "save":
+            save_savefile(savefile_path, checklist)
+        elif command == "clean":
+            for item in checklist:
+                if item.done:
+                    item.hidden = True
         else:
             logger.error(f"ERROR: Unknown command '{command}'")
             continue
